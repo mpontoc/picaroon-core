56ccb8aa35b29669294323fb4fa3ddea2b67fe55
familia <familia@DESKTOP-G5PN4CG>
2021-04-17 16:43:25 -0300

    create a spring project

---
+++ src/main/java/br/com/mpontoc/picarooncore/PicaroonCoreApplication.java
@@ -0,0 +1,15 @@
+package br.com.mpontoc.picarooncore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicaroonCoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicaroonCoreApplication.class, args);
+		
+		System.out.println("Hello World");
+	}
+
+}
---
+++ src/test/java/br/com/mpontoc/picarooncore/PicaroonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mpontoc.picarooncore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicaroonCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
